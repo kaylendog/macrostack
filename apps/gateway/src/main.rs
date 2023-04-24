2df321f1675130c47894a699080b4c8d9700f032
Kaylen Dart <actuallyori@gmail.com>
2023-04-24 21:40:42 +0100

    feat: add example service call

--- apps/gateway/src/main.rs
+++ apps/gateway/src/main.rs
@@ -1,13 +1,47 @@
+use std::{env, net::Ipv4Addr};
+
 use anyhow::Result;
+use macrostack_protocol::hello::{greeter_client::GreeterClient, HelloReply, HelloRequest};
+use serde::Serialize;
+use tonic::Response;
 use warp::Filter;
+
+#[derive(Serialize)]
+struct Message {
+    message: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     macrostack_tracing::init("example-service")?;
 
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    // greeter service config
+    let greeter_host: Ipv4Addr = env::var("EXAMPLE_SERVICE_CONNECT_SERVICE_HOST")?.parse()?;
+    let greeter_port: u16 = env::var("EXAMPLE_SERVICE_CONNECT_SERVICE_PORT")?.parse()?;
+    // connect to greeter service
+    let greeter = macrostack_protocol::hello::greeter_client::GreeterClient::connect(format!(
+        "http://{}:{}",
+        greeter_host, greeter_port
+    ))
+    .await?;
 
-    warp::serve(hello).run(([127, 0, 0, 1], 8080)).await;
+    let greeter = warp::any().map(move || greeter.clone());
+
+    let routes = warp::path!("hello" / String)
+        .and(greeter)
+        .and_then(|name, mut greeter: GreeterClient<_>| async move {
+            greeter
+                .say_hello(HelloRequest { name })
+                .await
+                .map_err(|_| warp::reject())
+        })
+        .map(|reply: Response<HelloReply>| {
+            warp::reply::json(&Message {
+                message: reply.into_inner().message,
+            })
+        });
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 
     Ok(())
 }
